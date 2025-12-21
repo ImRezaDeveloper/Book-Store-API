b3b8ddd5c22a8b931a52f57f7ea06e99056ce921
Reza <rezapapi138418@gmail.com>
2025-12-21 11:45:32 +0330

    commit for db

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-# from core.config import DATABAES_URL
 from app.core.config import settings
-
-# DATABASE_URL = "postgresql+asyncpg://reza:reza.papi1384@localhost:5432/bookstoreapi"
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
